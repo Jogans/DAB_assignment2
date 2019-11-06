b41c130ccd96780035308d862cf2c7aa8aa89df4
Jogans <201404212@post.au.dk>
2019-11-06 09:22:21 +0100

    Tilføjet ting

---
+++ Restuarant/Controllers/Views.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAB_Assignment_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAB_Assignment_2.DAL
+{
+    public class Views
+    {
+        public IEnumerable<Restaurant> MethodA(string type)
+        {
+            AppDbContext context = new AppDbContext();
+            RestaurantRepository restaurantRepository = new RestaurantRepository(context);
+            //context.Restaurants.Find
+            //context.Restaurants.Where(p => p.Type.StartsWith(type)).ToList();
+            var restaurants = context.Restaurants
+                .Where(p => p.Type.StartsWith(type))
+                .Include(p => p.Name)
+                .Include(p => p.AverageRating)
+                .Include(p => p.Reviews.
+                    Select(r => r.Text).Take(5))
+                .ToList();
+            return restaurants;
+        }
+
+        public IEnumerable<Restaurant> MethodB(string address)
+        {
+            AppDbContext context = new AppDbContext();
+            var restaurant = context.Restaurants
+                .Where(r => r.Address.StartsWith(address))
+                .Include(r => r.RestaurantDishes
+                    .Select(d => d.Dish)
+                    .Select(d => new {d.DishName, d.Price}))
+                .Include(r => r.AverageRating)
+                .ToList();
+            return restaurant;
+        }
+
+        public IEnumerable<Restaurant> MethodC(string address)
+        {
+            AppDbContext context = new AppDbContext();
+            var restaurants = context.Restaurants
+                .Where(r => r.Name.StartsWith(address))
+                .Include(r => r.Tables
+                    .Select(t => t.TableId))
+                .ToList();
+            return restaurants;
+        }
+
+        //public IEnumerable<Restaurant> MethodD(string address)
+        //{
+        //    AppDbContext context = new AppDbContext();
+        //    var restaurants = context.Restaurants
+        //        .Join(context.Guests, cu => cu.RestaurantId, p => p.)
+        //}
+    }
+}
---
+++ Restuarant/Controllers/WaiterRepository.cs
@@ -0,0 +1,66 @@
+﻿using DAB_Assignment_2.Models;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace DAB_Assignment_2.DAL
+{
+    public class WaiterRepository : IDisposable
+    {
+        private AppDbContext context;
+
+        public WaiterRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Waiter> GetWaiters()
+        {
+            return context.Waiters.ToList();
+        }
+
+        public Waiter GetWaiterByID(int id)
+        {
+            return context.Waiters.Find(id);
+        }
+
+        public void InsertWaiter(Waiter waiter)
+        {
+            context.Waiters.Add(waiter);
+        }
+
+        public void DeleteTable(int Id)
+        {
+            Waiter waiter = context.Waiters.Find(Id);
+            context.Waiters.Remove(waiter);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
