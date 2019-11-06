62f191db01e7639bf328502187aa787659a1fd92
salling008 <frederiksalling8@hotmail.com>
2019-11-06 16:20:23 +0100

    SaNgEn StArTeR IkKe mEd A

--- Restuarant/Data/AppDbContext .cs
+++ Restuarant/Data/AppDbContext .cs
@@ -17,6 +17,9 @@ namespace Restaurant.Data
         public DbSet<Table> Tables { get; set; }
         public DbSet<Dish> Dishes { get; set; }
         public DbSet<Review> Reviews { get; set; }
+        public DbSet<GuestDish> GuestDishes { get; set; }
+        public DbSet<RestaurantDish> RestaurantDishes { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
